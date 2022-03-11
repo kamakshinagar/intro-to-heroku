9740ee1dff134bdfc2193e08c02808f653803d5e
kamakshinagar <93729666+kamakshinagar@users.noreply.github.com>
2022-03-11 14:49:40 -0800

    Update release-tasks.sh

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,1 +1,12 @@
-heroku ps:scale web=2
+$ curl -n -X PATCH https://api.heroku.com/apps/$sleepy-reaches-65865/formation \
+  -d '{
+  "updates": [
+    {
+      "quantity": 2,
+      "size": "standard-1X",
+      "type": "web"
+    }
+  ]
+}' \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/vnd.heroku+json; version=3"
